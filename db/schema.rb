bb54cac8f49dbeed586a10fc0a75993532de88c5
Ryuta0210 <ryutoyoda0210@gmail.com>
2024-08-12 23:44:26 +0900

    メッセージ送信機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_12_121856) do
   create_table "messages", charset: "utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "room_id", null: false
+    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["room_id"], name: "index_messages_on_room_id"
